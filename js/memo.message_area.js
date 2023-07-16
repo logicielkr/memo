899cbdf4b76b405367baf415678b622510289f4b
logicielkr <graha@graha.kr>
2023-07-17 00:27:07 +0900

    1. Firefox legacy 버전 호환성을 위해 marked 및 highlight.js 버전을 변경
    2. 상세보기 화면의 highlightjs-line-numbers.js 관련 여백 및 줄바꿈 변경
    
    3. MessageArea 에서 message 함수 추가 및 명백하게 잘못된 코드 수정

--- js/memo.message_area.js
+++ js/memo.message_area.js
@@ -1,7 +1,8 @@
 function MessageArea() {
 }
-MessageArea.hide = function(callback, cancleCallback) {
+MessageArea.hide = function() {
 	$("div#message_area").hide();
+	$("div#message_area div.btn div.btns").hide();
 	$(window).off("scroll");
 	$(window).off("keypress");
 	$(window).off("keydown");
@@ -12,12 +13,14 @@ MessageArea.alert = function(out, callback) {
 			$("div#message_area").css("top", $(window).scrollTop());
 	});
 	$("div#message_area div.confirm").hide();
+	$("div#message_area div.btn div.btns").hide();
 	$("div#message_area ul.alert").empty();
 	for(var i = 0; i < out.length; i++) {
 		$("div#message_area ul.alert").append("<li>" + out[i].msg + "</li>")
 	}
 	$("div#message_area input.cancle").hide();
 	$("div#message_area input.confirm").off("click");
+	$("div#message_area input.cancle").off("click");
 	$("div#message_area input.confirm").click(function() {
 		MessageArea.hide();
 		if(callback) {
@@ -45,9 +48,10 @@ MessageArea.confirm = function(msg, callback, cancleCallback) {
 			$("div#message_area").css("top", $(window).scrollTop());
 	});
 	$("div#message_area ul.alert").hide();
+	$("div#message_area div.btn div.btns").hide();
 	
 	$("div#message_area div.confirm").empty();
-	$("div#message_area div.confirm").append(msg)
+	$("div#message_area div.confirm").append(msg);
 	
 	$("div#message_area input.confirm").off("click");
 	$("div#message_area input.confirm").click(function() {
@@ -90,7 +94,43 @@ MessageArea.confirm = function(msg, callback, cancleCallback) {
 		}
     });
 	$("div#message_area input.cancle").show();
+	$("div#message_area input.confirm").show();
 	
 	$("div#message_area div.confirm").show();
+	$("div#message_area").show();
+};
+
+MessageArea.message = function(msg, btns) {
+	$("div#message_area").css("top", $(window).scrollTop());
+	$(window).scroll(function() {
+			$("div#message_area").css("top", $(window).scrollTop());
+	});
+	$("div#message_area ul.alert").hide();
+	
+	$("div#message_area div.confirm").empty();
+	$("div#message_area div.confirm").append(msg);
+	
+	$("div#message_area input.confirm").off("click");
+	$("div#message_area input.cancle").off("click");
+
+	$("div#message_area input.confirm").hide();
+	$("div#message_area input.cancle").hide();
+	if(btns) {
+		$("div#message_area div.btn div.btns").empty();
+		for(var i = 0; i < btns.length; i++) {
+			var result = $("div#message_area div.btn div.btns").append("<input type=\"button\" value=\"" + btns[i].label + "\" class=\"" + btns[i].className + "\" />");
+			if(btns[i].callback) {
+				result.children().last().click(
+					{
+						callback:btns[i].callback
+					},
+					function(event) {
+						event.data.callback();
+					}
+				);
+			}
+		}
+	}
+	$("div#message_area div.btn div.btns").show();
 	$("div#message_area").show();
 };
